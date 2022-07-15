d1a99598c9d9a53acf023514a97dcfa44da54c42
Ryan Trinh <rqtrinh@cpp.edu>
2022-07-15 02:50:04 +0000

    Correct name discrepancies to get json data from datastore

--- src/main/java/com/google/sps/data/Post.java
+++ src/main/java/com/google/sps/data/Post.java
@@ -7,15 +7,19 @@ public final class Post {
   private final String BREED;
   private final String AGE;
   private final String LOCATION;
-  private final String CONTACT_INFO;
-  private final String IMAGE;
+  private final String NAME;
+  private final String EMAIL;
+  private final String PHONE;
+  private final String PIC;
 
-  public Post(String petType, String breed, String age, String location, String contact_info, String image) {
+  public Post(String petType, String breed, String age, String location, String name, String email, String phone, String pic) {
     this.PET_TYPE = petType;
     this.BREED = breed;
     this.AGE = age;
     this.LOCATION = location;
-    this.CONTACT_INFO = contact_info;
-    this.IMAGE = image;
+    this.NAME = name;
+    this.EMAIL = email;
+    this.PHONE = phone;
+    this.PIC = pic;
   }
 }
--- src/main/java/com/google/sps/servlets/HomeServlet.java
+++ src/main/java/com/google/sps/servlets/HomeServlet.java
@@ -15,7 +15,6 @@ import com.google.cloud.datastore.Query;
 import com.google.cloud.datastore.QueryResults;
 import com.google.cloud.datastore.StructuredQuery.OrderBy;
 import com.google.gson.Gson;
-import com.google.cloud.datastore.Blob;
 import java.util.ArrayList;
 import java.util.List;
 import com.google.sps.data.Post;
@@ -27,24 +26,26 @@ public class HomeServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
     response.setContentType("text/html;");
-    request.getRequestDispatcher("../webapp/index.html").forward(request, response); 
+    //request.getRequestDispatcher("../webapp/index.html").forward(request, response); 
     
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
     Query<Entity> query =
-        Query.newEntityQueryBuilder().setKind("Post").setOrderBy(OrderBy.asc("location")).build();
+        Query.newEntityQueryBuilder().setKind("post").setOrderBy(OrderBy.asc("location")).build();
     QueryResults<Entity> results = datastore.run(query);
 
     List<Post> posts = new ArrayList<>();
     while (results.hasNext()) {
       Entity entity = results.next();
-      String petType = entity.getString("PET_TYPE");
-      String breed = entity.getString("BREED");
-      String age = entity.getString("AGE");
-      String location = entity.getString("LOCATION");
-      String contactInfo = entity.getString("CONTACT_INFO");
-      String image = entity.getString("IMAGE");
+      String petType = entity.getString("petType");
+      String breed = entity.getString("breed");
+      String age = entity.getString("age");
+      String location = entity.getString("location");
+      String name = entity.getString("name");
+      String email = entity.getString("email");
+      String phone = entity.getString("phone");
+      String pic = entity.getString("pic");
 
-      Post post = new Post(petType, breed, age, location, contactInfo, image);
+      Post post = new Post(petType, breed, age, location, name, email, phone, pic);
       posts.add(post);
     }
 
