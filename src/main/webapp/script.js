9f6fb332964f973fa8cde29eaffa89dbd81ff57e
Ryan Trinh <rqtrinh@cpp.edu>
2022-07-17 18:15:39 +0000

    Dynamically load HTML pet cards with JS

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -15,5 +15,25 @@ async function showPets() {
     const responseFromServer = await fetch('/home');
     const textFromResponse = await responseFromServer.json();
 
-    alert(textFromResponse[0].NAME);
+    for(let i=0; i<3; i++)
+    {
+        //Getting the id of each card
+        let picID = "pic"+ i;
+        let petTypeID = "petType" + i;
+        let breedID = "breed" + i;
+        let ageID = "age" + i;
+        let locationID = "location" + i;
+        let nameID = "name" + i;
+        let emailID = "email" + i;
+
+        //Update each card in the HTML
+        document.getElementById(picID).src = "data:imagejpeg;base64," + textFromResponse[i].PIC;
+        document.getElementById(petTypeID).innerHTML = textFromResponse[i].PET_TYPE;
+        document.getElementById(breedID).innerHTML = textFromResponse[i].BREED;
+        document.getElementById(ageID).innerHTML = textFromResponse[i].AGE;
+        document.getElementById(locationID).innerHTML = textFromResponse[i].LOCATION;
+        document.getElementById(nameID).innerHTML = textFromResponse[i].NAME;
+        document.getElementById(emailID).innerHTML = textFromResponse[i].EMAIL;
+
+    }
 }
