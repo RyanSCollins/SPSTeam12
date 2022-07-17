c7b658e6711138e12d26938d05d810ea5b4c39f7
nandini19111 <106172184+nandini19111@users.noreply.github.com>
2022-07-17 16:30:31 +0800

    Update PetPostServlet.java

--- src/main/java/com/google/sps/servlets/PetPostServlet.java
+++ src/main/java/com/google/sps/servlets/PetPostServlet.java
@@ -1,8 +1,7 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Base64;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,12 +13,13 @@ import javax.servlet.http.Part;
 
 import com.google.cloud.datastore.Entity;
 import com.google.cloud.datastore.FullEntity;
-import com.google.cloud.datastore.Blob;
-import com.google.cloud.datastore.BlobValue;
 import com.google.cloud.datastore.Datastore;
 import com.google.cloud.datastore.DatastoreOptions;
 import com.google.cloud.datastore.KeyFactory;
+import com.google.cloud.datastore.StringValue;
+import com.google.sps.data.PostEntity;
 
+import org.apache.commons.io.IOUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Safelist;
 
@@ -27,70 +27,46 @@ import org.jsoup.safety.Safelist;
 @MultipartConfig(fileSizeThreshold=1024*1024*2, // 2MB
                 maxFileSize=1024*1024*10,      // 10MB
                 maxRequestSize=1024*1024*50)   // 50MB
+
 public class PetPostServlet extends HttpServlet {
-  //Constants for use across methods
-  public static final String POST = "post";
-  public static final String PET_TYPE = "petType";
-  public static final String BREED = "breed";
-  public static final String AGE = "age";
-  public static final String LOCATION = "location";
-  public static final String PIC = "pic";
-  public static final String NAME = "name";
-  public static final String EMAIL = "email";
-  public static final String PHONE = "phone";
-
-
-
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
     //Information requested in the form
-    String petType = Jsoup.clean(request.getParameter(PET_TYPE), Safelist.none()) ;
-    String breed = Jsoup.clean(request.getParameter(BREED), Safelist.none());
-    String age = Jsoup.clean(request.getParameter(AGE), Safelist.none());
-    String location = Jsoup.clean(request.getParameter(LOCATION), Safelist.none());
-    String name = Jsoup.clean(request.getParameter(NAME), Safelist.none()); 
-    String email = Jsoup.clean(request.getParameter(EMAIL), Safelist.none()); 
-    String phone = Jsoup.clean(request.getParameter(PHONE), Safelist.none()); 
+    String petType = Jsoup.clean(request.getParameter(PostEntity.PET_TYPE), Safelist.none()) ;
+    String breed = Jsoup.clean(request.getParameter(PostEntity.BREED), Safelist.none());
+    String age = Jsoup.clean(request.getParameter(PostEntity.AGE), Safelist.none());
+    String location = Jsoup.clean(request.getParameter(PostEntity.LOCATION), Safelist.none());
+    String name = Jsoup.clean(request.getParameter(PostEntity.NAME), Safelist.none()); 
+    String email = Jsoup.clean(request.getParameter(PostEntity.EMAIL), Safelist.none()); 
+    String phone = Jsoup.clean(request.getParameter(PostEntity.PHONE), Safelist.none()); 
 
 
 
     //Get the picPart of the data
-    Part pic = request.getPart(PIC);
+    Part pic = request.getPart(PostEntity.PIC);
     //Convert pic to blob data
-    String picString = getValue(pic);
-    byte[] byteData = picString.getBytes();
-    Blob picBlob = Blob.copyFrom(byteData);
+    byte[] byteData = IOUtils.toByteArray(pic.getInputStream());
+    String picBase64 = Base64.getEncoder().encodeToString(byteData);
 
     //Save the information in the database
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
-    KeyFactory keyFactory = datastore.newKeyFactory().setKind(POST);
+    KeyFactory keyFactory = datastore.newKeyFactory().setKind(PostEntity.POST);
     FullEntity postEntity = 
     Entity.newBuilder(keyFactory.newKey()) 
-        .set(PET_TYPE, petType)
-        .set(BREED, breed)
-        .set(AGE, age)
-        .set(LOCATION, location)
-        .set(PIC, BlobValue.newBuilder(picBlob).setExcludeFromIndexes(true).build())
-        .set(NAME, name)
-        .set(EMAIL, email)
-        .set(PHONE, phone)
+        .set(PostEntity.PET_TYPE, petType)
+        .set(PostEntity.BREED, breed)
+        .set(PostEntity.AGE, age)
+        .set(PostEntity.LOCATION, location)
+        .set(PostEntity.PIC, StringValue.newBuilder(picBase64).setExcludeFromIndexes(true).build())
+        .set(PostEntity.NAME, name)
+        .set(PostEntity.EMAIL, email)
+        .set(PostEntity.PHONE, phone)
         .build();
     datastore.put(postEntity);
 
     //Redirect to home page
     String baseUrl = request.getRequestURL().substring(0, request.getRequestURL().length() - request.getRequestURI().length()) + request.getContextPath();
     response.sendRedirect(baseUrl);
-  }
-
-  //Gets the string value of a part
-  private static String getValue(Part part) throws IOException {
-    BufferedReader reader = new BufferedReader(new InputStreamReader(part.getInputStream(), "UTF-8"));
-    StringBuilder value = new StringBuilder();
-    char[] buffer = new char[1024];
-    for (int length = 0; (length = reader.read(buffer)) > 0;) {
-      value.append(buffer, 0, length);
-    }
-    return value.toString();
   }
 }
